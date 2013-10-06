aacde9ca57885731a28c5a13be0a4484c4df8d44
Vyacheslav <evangi.studio@hotmail.com>
2013-10-06 22:05:52 +0400

    Base Simple Membership

---
+++ SimpleMembershipExample/Models/SMDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace SimpleMembershipExample.Models
+{
+    public class SMDbContext : DbContext
+    {
+        public SMDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
